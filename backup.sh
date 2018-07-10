e41edbf0aa447d638a2593acd53b14c1ca3d66a7
mocu <curzio.moriggia@protonmail.ch>
2018-07-10 20:43:51 +0200

    Delete backup.sh

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-zip -qr smbdrive_bellinzona.zip /media/DLNA/buero
-ncftp -u cumodmoz -p PipperoP3 192.168.0.248 <<END_SCRIPT
-cd Public/Backups/SAMBA
-put /home/moz/smbdrive_bellinzona.zip 
-END_SCRIPT
-rm -r /home/moz/smbdrive_bellinzona.zip
-
